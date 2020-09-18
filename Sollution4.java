2ada4dd2e0ea3f3010b0d84696dadb3c9923b433
Edg4rV <vardedgar@mail.ru>
2020-09-18 17:37:04 +0400

    Create Sollution4.java

---
+++ Sollution4.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        while (scanner.hasNext()) {
+            int number = scanner.nextInt();
+            executor.submit(new PrintIfPrimeTask(number));
+        }
+        executor.shutdown();
+    }
+}
+
+class PrintIfPrimeTask implements Runnable {
+    private final int number;
+
+    public PrintIfPrimeTask(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public void run() {
+        if (isPrime(number)) {
+            System.out.println(number);
+        }
+    }
+
+    boolean isPrime(int n) {
+        if (n == 1) {
+            return false;
+        }
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
